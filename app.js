a332c7080b6e3a36a59b27759e8ec6dc9280d9bd
soraluz <rijasoag23@hotmail.com>
2024-04-16 18:36:48 -0500

    usar target en vez de la referencia

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const p_rojo= document.getElementById("texto-rojo")
 const i_rojo= document.getElementById("rojo")
 p_rojo.innerText = i_rojo.value
 
-i_rojo.addEventListener("change",()=>{
-    p_rojo.innerText = i_rojo.value
+i_rojo.addEventListener("change",(e)=>{
+    p_rojo.innerText = e.target.value
+    //p_rojo.innerText = i_rojo.value
     document.body.style.backgroundColor= `rgb(${i_rojo.value},${i_verde.value},${i_azul.value})`
 })
 
@@ -13,8 +14,9 @@ const i_verde= document.getElementById("verde")
 p_verde.innerText = i_verde.value
 
 
-i_verde.addEventListener("change",()=>{
-    p_verde.innerText = i_verde.value
+i_verde.addEventListener("change",(e)=>{
+    p_verde.innerText = e.target.value
+    //p_verde.innerText = i_verde.value
     document.body.style.backgroundColor= `rgb(${i_rojo.value},${i_verde.value},${i_azul.value})`
 })
 
@@ -25,7 +27,8 @@ const i_azul= document.getElementById("azul")
 p_azul.innerText = i_azul.value
 
 
-i_azul.addEventListener("change",()=>{
-    p_azul.innerText = i_azul.value
+i_azul.addEventListener("change",(e)=>{
+    p_azul.innerText = e.target.value
+    //p_azul.innerText = i_azul.value
     document.body.style.backgroundColor= `rgb(${i_rojo.value},${i_verde.value},${i_azul.value})`
 })
