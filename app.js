68f47ea16e72f9aeb3951dc629e5ffe128e07133
soraluz <rijasoag23@hotmail.com>
2024-04-16 18:27:44 -0500

    Javascript colores RGB

---
+++ app.js
@@ -0,0 +1,31 @@
+const p_rojo= document.getElementById("texto-rojo")
+const i_rojo= document.getElementById("rojo")
+p_rojo.innerText = i_rojo.value
+
+i_rojo.addEventListener("change",()=>{
+    p_rojo.innerText = i_rojo.value
+    document.body.style.backgroundColor= `rgb(${i_rojo.value},${i_verde.value},${i_azul.value})`
+})
+
+const p_verde= document.getElementById("texto-verde")
+const i_verde= document.getElementById("verde")
+
+p_verde.innerText = i_verde.value
+
+
+i_verde.addEventListener("change",()=>{
+    p_verde.innerText = i_verde.value
+    document.body.style.backgroundColor= `rgb(${i_rojo.value},${i_verde.value},${i_azul.value})`
+})
+
+
+const p_azul= document.getElementById("texto-azul")
+const i_azul= document.getElementById("azul")
+
+p_azul.innerText = i_azul.value
+
+
+i_azul.addEventListener("change",()=>{
+    p_azul.innerText = i_azul.value
+    document.body.style.backgroundColor= `rgb(${i_rojo.value},${i_verde.value},${i_azul.value})`
+})
